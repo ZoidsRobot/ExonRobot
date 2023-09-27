b42f63ecb5b03bb6248e341b6c1efa35a8dcaa12
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-09-28 05:32:38 +0700

    Update Callback.py

--- Exon/modules/Callback.py
+++ Exon/modules/Callback.py
@@ -131,7 +131,7 @@ def ABG_about_callback(update, context):
                             text="sᴜᴘᴘᴏʀᴛ", url=f"t.me/{SUPPORT_CHAT}"
                         ),
                         InlineKeyboardButton(
-                            text="ᴜᴘᴅᴀᴛᴇs", url="https://t.me/Abishnoi_bots"
+                            text="ᴜᴘᴅᴀᴛᴇs", url="https://t.me/ZOIDs_Robot"
                         ),
                     ],
                     [
