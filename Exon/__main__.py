32066c58b3885aa423ec1a55be01d9e6323e00db
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-09-28 03:51:26 +0700

    Update __main__.py

--- Exon/__main__.py
+++ Exon/__main__.py
@@ -802,7 +802,7 @@ def main():
                 parse_mode=ParseMode.MARKDOWN,
             )
         except Unauthorized:
-            LOGGER.warning(
+            log.warning(
                 f"Bot isn't able to send message to @{SUPPORT_CHAT}, go and check!"
             )
         except BadRequest as e:
