5411b0b89b295f63a7ceff985c62731dfd1375c0
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-09-28 04:30:46 +0700

    Update __main__.py

--- Exon/__main__.py
+++ Exon/__main__.py
@@ -251,32 +251,28 @@ def start(update: Update, context: CallbackContext):  # sourcery no-metrics
 
         else:
             first_name = update.effective_user.first_name
-            usr = update.effective_user
-            lol = update.effective_message.reply_text(
-                PM_START_TEX.format(usr.first_name), parse_mode=ParseMode.MARKDOWN
-            )
-            time.sleep(0.4)
-            lol.edit_text("🎊")
-            time.sleep(0.5)
-            lol.edit_text("⚡")
-            time.sleep(0.3)
-            lol.edit_text("ꜱᴛᴀʀᴛɪɴɢ... ")
-            time.sleep(0.4)
-            lol.delete()
             update.effective_message.reply_text(
-                text=gs(chat.id, "pm_start_text").format(
+                PM_START_TEXT.format(
                     escape_markdown(first_name),
-                    escape_markdown(context.bot.first_name),
+                    BOT_NAME,
                     escape_markdown(uptime),
-                    sql.num_chat_users(),
-                    sql.num_chats(),
-                    OWNER_ID,
-                ),
+                    sql.num_users(),
+                    sql.num_chats()),                        
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
                 disable_web_page_preview=False,
             )
+    else:
+        update.effective_message.reply_photo(
+            START_IMG,
+            caption="ʜɪ, ɪ,ᴍ ᴢᴏɪᴅ x ʀᴏʙᴏᴛ sᴇɴᴀɴɢ ʙᴇʀᴛᴇᴍᴜ ᴅᴇɴɢᴀɴ ᴍᴜ \n<b>  :</b> <code>{}</code>".format(
+                uptime
+            ),
+            parse_mode=ParseMode.HTML,
+        )
+
+    
 
     else:
         update.effective_message.reply_text(gs(chat.id, "grp_start_text"))
