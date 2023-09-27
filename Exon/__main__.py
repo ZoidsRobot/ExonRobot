dcbdde96c424055a6a08004263f99645e418f506
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-09-28 04:17:47 +0700

    Update __main__.py

--- Exon/__main__.py
+++ Exon/__main__.py
@@ -704,7 +704,7 @@ def migrate_chats(update: Update, context: CallbackContext):
 def main():
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendphoto(
+            dispatcher.bot.send_photo(
                 f"@{SUPPORT_CHAT}",
                 photo=START_IMG,
                 caption=f"""
